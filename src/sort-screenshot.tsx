4e2bff7858e70d0e0fe6d19660d770c645fabaf3
Jed Sweeney <jedsweeney5@gmail.com>
2026-04-07 18:48:59 -0400

    feat: working directory selector

--- src/sort-screenshot.tsx
+++ src/sort-screenshot.tsx
@@ -1,10 +1,76 @@
-import { ActionPanel, Action, Icon, List, closeMainWindow } from "@raycast/api";
+import { ActionPanel, Action, Icon, List, Cache } from "@raycast/api";
+import { useCachedState } from "@raycast/utils"; 
+import { useState } from "react"; 
+import * as os from "os"
 import fs from "fs"; 
 
-export default function Command() {
-	// Need to add a config option 
-	// wherever they want the screenshots to go thats the dir I should open
-	<List>
+const cache = new Cache(); 
 
-	</List>
+export default function Command() {
+	const [dir, setDir] = useCachedState("")
+
+	const callback = (directory) => { setDir(directory); }
+
+	if (dir) {
+		return (
+			<List>
+			<List.Item 
+			title="Augustiner Helles"
+			actions={
+				<ActionPanel>
+				<Action title="Select" onAction={() => console.log("TESTING")} />
+				</ActionPanel>
+			}
+			/>
+			</List>
+		)
+	} else {
+		return (
+			<DirectoryList 
+				directory={os.homedir()}
+				callback={callback}
+			/>
+		)
+	}
+}
+
+// Displays the directory list for the selected directory
+function DirectoryList(props) {
+  const [directory, setDirectory] = useState(props.directory);
+  const files = getDirectories(directory);
+
+  return (
+    <List>
+      <List.Item title="Select Your Screenshot Save Directory with 'cmd .'" />
+      {files.map((item) => (
+        <List.Item
+          key={item}
+          title={item}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Select"
+                onAction={() => {
+                  setDirectory(directory + "/" + item);
+                }}
+				/>
+              <Action
+                title="Confirm Directory"
+			  	shortcut={{ modifiers: ["cmd"], key: "." }}
+                onAction={() => {
+					props.callback(directory); 
+                }}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
+
+function getDirectories(directory) {
+	return fs.readdirSync(directory).filter((f) => {
+		return !f.startsWith(".") && fs.lstatSync(directory + "/" + f).isDirectory();
+	}); 
 }
