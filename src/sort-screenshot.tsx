c645db933155dc1774baf7a22ca80c63bc2db106
Jed Sweeney <jedsweeney5@gmail.com>
2026-04-06 21:02:05 -0400

    feat: screenshot behavior + directory storing

---
+++ src/sort-screenshot.tsx
@@ -0,0 +1,10 @@
+import { ActionPanel, Action, Icon, List, closeMainWindow } from "@raycast/api";
+import fs from "fs"; 
+
+export default function Command() {
+	// Need to add a config option 
+	// wherever they want the screenshots to go thats the dir I should open
+	<List>
+
+	</List>
+}
--- src/take-a-screenshot.tsx
+++ src/take-a-screenshot.tsx
@@ -1,49 +1,26 @@
-import { ActionPanel, Action, Icon, List } from "@raycast/api";
-import { useExec } from "@raycast/utils";
-const { exec } = require('node:child_process'); 
+import { closeMainWindow, launchCommand, LaunchType } from "@raycast/api"; 
+import { promisify } from "util";
+import { exec } from "child_process";
+import fs from "fs";
+const path = require("node:path"); 
 
-/* 
-	The Plan 
-When the command runs we immediately want to call the screenshot command in macos 
-As an MVP lets just try to rename the screenshot. 
-	After the screenshot is complete we want to popup the list of ITEMS. 
-	This list of ITEMS is going to be a collection of folders held within screenshots 
-Basically we are going to explore all the directories until we find a directory called screenshot. 
-	When we find that directory we are going to fetch the folders in that directory and those folders 
-are going to populate the list modal 
-After the user makes their selection we are going to save the screenshot to the folder specified.
+const execAsync = promisify(exec);
 
-	*/ 
+function getUniqueFileName(): string { 
+	const files = fs.readdirSync("/tmp/raycast-screenshots"); 
+	const names = files.map((file) => path.parse(file).name); 
+	let randomFileName: string;
+	do {
+		randomFileName = Math.random().toString(36).slice(2); 
+	} while (names.includes(randomFileName)); 
 
-const ITEMS = Array.from(Array(3).keys()).map((key) => {
-	return {
-		id: key,
-		icon: Icon.Bird,
-		title: "Title " + key,
-		subtitle: "Subtitle",
-		accessory: "Accessory",
-	};
-});
+	return randomFileName;
+}
 
-export default function Command() {
-	const { isLoading, data } = useExec('screencapture', ['-i', '~/Desktop/screenshot.png']); 
-	console.log({isLoading}); 
-		return (
-			<List isLoading={isLoading}>
-			{ !isLoading && ITEMS.map((item) => (
-				<List.Item
-				key={item.id}
-				icon={Icon.Text}
-				title={item.title}
-				subtitle={item.subtitle}
-				accessories={[{ icon: Icon.Text, text: item.accessory }]}
-				actions={
-					<ActionPanel>
-					<Action.CopyToClipboard content={item.title} />
-					</ActionPanel>
-				}
-				/>
-			))}
-			</List>
-		);
+export default async function Command() {
+	await closeMainWindow({ clearRootSearch: true }); 
+	await execAsync('mkdir -p /tmp/raycast-screenshots'); 
+	let name = getUniqueFileName(); 
+	await execAsync(`/usr/sbin/screencapture -i /tmp/raycast-screenshots/${name}.png`);
+	await launchCommand({ name: "sort-screenshot", type: LaunchType.UserInitiated });
 }
